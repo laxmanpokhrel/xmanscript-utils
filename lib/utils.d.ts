2877f3f3288fe0d3327459f1c49e73f16ff61ba0
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-09-05 09:02:05 +0545

    build: package

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -12,9 +12,16 @@ export declare function setKeyToZeroInObjects(arr: Record<string, any>[], key: s
 export declare function setKeysToZeroInObjects(arr: Record<string, any>[], keys: string[]): Record<string, any>[];
 export declare function toggleStringInArray(arr: string[], targetString: string): string[];
 export declare function toggleObjectInArray<T>(arr: T[], targetObject: T): T[];
-export declare function calculateAndInjectPercentage(arr: Record<string, any>[], key: string): Record<string, any>[];
+export declare function calculateAndInjectPercentageByMaxValue(
+  arr: Record<string, any>[],
+  key: string,
+): Record<string, any>[];
+export declare function calculateAndInjectPercentageBySum(
+  arr: Record<string, any>[],
+  key: string,
+): Record<string, any>[];
 export declare function calculateSumOfKey(data: Record<string, any>[], key: string): number;
-export declare function setZeroValueForMatchingObjects(
+export declare function setZeroValueForMatchingValuesOfAKey(
   arr: Record<string, any>[],
   key: string,
   values: string[],
@@ -24,8 +31,6 @@ type scrollToComponentProps = {
   focusAfterScroll: boolean;
 };
 export declare function scrollToComponent({ componentId, focusAfterScroll }: scrollToComponentProps): void;
-export declare function replaceObjectAtPosition<T>(arr: T[], position: number, newObj: T): T[];
-export declare function removeObjectAtIndex<T>(array: T[], index: number): T[];
 export declare function convertNestedKeysToObject(input: Record<string, any>): any;
 export declare function distributePercentageEquallyWithRemainder(array: Record<string, any>[]): Record<string, any>[];
 export declare function markCheckedByStringMatch<T>(objects: T[], strings: string[], chooseKey: keyof T): T[];
@@ -36,14 +41,14 @@ export declare function removeObjectFromArray(
   objects: Record<string, any>[],
   object: Record<string, any>,
 ): Record<string, any>[];
-export declare function parseToBollean(val: string): boolean;
+export declare function parseToBollean(val: string): boolean | undefined;
 export declare function groupArrayOfObjectsByValueOfAKey(
   arr: Record<string, any>[],
   key: string,
 ): Record<string, any>[][];
 export declare function countKeyOccurrences(json: any, key: string): number;
-export declare function dividePercentage(json: any, key: string): any;
-export declare function uniqueValuesForKey(data: Record<string, any>[], key: string): string[];
+export declare function distributePercentageEquall(json: any, key: string): any;
+export declare function uniqueValuesOfKey(data: Record<string, any>[], key: string): string[];
 export declare function formatNumberToCommaString(value: number): string;
 export declare function getMinMax(
   arr: any[],
@@ -52,5 +57,4 @@ export declare function getMinMax(
   min: any;
   max: any;
 };
-export declare function calculateItemPercentages(arr: any[]): any[];
 export {};
