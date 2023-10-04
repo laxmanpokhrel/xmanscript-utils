a0e4320b9b09a86f41e48bc042a82a12c5e855e2
mridulgelal <mridul.gelal@gmail.com>
2023-10-04 23:22:33 +0545

    feat: dynamic after scroll position

--- src/utils.ts
+++ src/utils.ts
@@ -351,17 +351,23 @@ export function setZeroValueForMatchingValuesOfAKey(
   });
 }
 
+type ScrollLogicalPosition = 'center' | 'end' | 'nearest' | 'start';
+
 type scrollToComponentProps = {
   componentId: string;
   focusAfterScroll?: boolean;
   scrollDelay?: number;
   focusDelay?: number;
+  align?: ScrollLogicalPosition;
 };
 
 /**
  * The scrollToComponent function scrolls to a specified component on the page and optionally focuses
  * on it after scrolling.
  * @param {scrollToComponentProps}  - - `componentId`: The id of the component to scroll to.
+ *  'align': The alignment of the component after scrolling. It can be one of the following values:
+ * 'start', 'center', or 'end'. The default value is 'center'.
+ * 'delay': The delay in milliseconds before scrolling to the component.
  */
 
 export function scrollToComponent({
@@ -369,6 +375,7 @@ export function scrollToComponent({
   focusAfterScroll = false,
   scrollDelay,
   focusDelay,
+  align = 'center',
 }: scrollToComponentProps) {
   // eslint-disable-next-line no-undef
   const element = document.getElementById(componentId);
@@ -379,7 +386,7 @@ export function scrollToComponent({
       setTimeout(() => {
         element.scrollIntoView({
           behavior: 'smooth',
-          block: 'center',
+          block: align,
         });
       }, scrollDelay);
     }
